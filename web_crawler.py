39f424545a09e266c512af3d8d741a3b33e94ddc
benwu95 <benwu95@gmail.com>
2018-04-19 12:27:31 +0800

    Part2 is finished.(12:27 p.m.)

--- web_crawler.py
+++ web_crawler.py
@@ -20,19 +20,40 @@ class WebCrawler(object):
         crawler_list = []
         for title in title_list:
             url_list.append('https://www.ptt.cc' + title.get('href'))
+    
         for url in url_list:
+            print('[crawling...] ' + url)
             s = BeautifulSoup(requests.get(url, cookies={'over18': '1'}).text, 'html.parser')
-            value = s.select('article-meta-value')
-            author = value[0].text
-            board = value[1].text
-            head = value[2].text
-            date = value[3].text
+            if s.select('span.article-meta-value'):
+                author = s.select('span.article-meta-value')[0].text
+                board = s.select('span.article-meta-value')[1].text
+                head = s.select('span.article-meta-value')[2].text
+                date = s.select('span.article-meta-value')[3].text
+                text = s.select('#main-content')[0].text
+                crawler_list.append(WebCrawler(author, board, head, date, text, url))
 
+        return crawler_list
 
 def main():
     board = input('Input the board name: ')
-    print(board)
-    WebCrawler.ptt_crawler(board)
+    crawler = WebCrawler.ptt_crawler(board)
+    count = 0
+    for item in crawler:
+        print('[' + str(count) + '] ' + item.head)
+        count += 1
+
+    while 1:
+        choice = input('input a list number to get detail information or input q to quit: ')
+        if choice == 'q':
+            break
+        else:
+            index = int(choice)
+            print(crawler[index].url)
+            print(crawler[index].board)
+            print(crawler[index].author)
+            print(crawler[index].head)
+            print(crawler[index].date)
+            print(crawler[index].text)
 
 
 if __name__ == '__main__':
